86f7fe93556efaff117aa84add53cb4b56b511ff
Ali Akpinar <ali2.akpinar.42@gmail.com>
2022-11-01 12:29:28 +0100

    ali nin dosyasi

--- src/day0/kemal.java
+++ src/day0/kemal.java
@@ -3,5 +3,6 @@ package day0;
 public class kemal {
     public static void main(String[] args) {
         System.out.println("kemal yayil");
+        System.out.println("yakalarsam operim kemal");
     }
 }
